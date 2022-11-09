e59546be004913e0bc5b0098ce709699113d4c82
simca09 <delacruz.klydean@yahoo.com>
2022-11-08 20:18:10 -0500

    Had to look outside for help but fairly easy

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
+const repeatString = function(string, int) {
+let repString = "";
+while (int > 0) {
+    repString += string;
+    int--;
+} if (int < 0) {
+    repString = "ERROR";
+}
+return repString;
+}
 
-};
+repeatString("hey", 3);
+repeatString("hey", 10);
+repeatString("hey", 1);
+repeatString("hey", 0);
+repeatString("hey", -1);
+const number = Math.floor(Math.random() * 1000);
+repeatString("hey", number);
+repeatString("", 10);
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
