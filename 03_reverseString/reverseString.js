857840597e51f4115c3fba0811d11f4ee98e8d6c
simca09 <delacruz.klydean@yahoo.com>
2022-11-08 20:44:37 -0500

    The split, reverse, join section

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+return string.split("").reverse().join("");
+}
 
-};
+reverseString("hello");
+reverseString("hello there");
+reverseString("123! abc!");
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;
