da1572f9a039c0085436ae6aa713ca5fbb3f3b10
simca09 <delacruz.klydean@yahoo.com>
2022-11-10 11:41:29 -0500

    temp conversation done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(x) {
+return Math.round((x - 32) * (5/9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(y) {
+return Math.round(((y * 9/5) + 32) *10) / 10;
 };
 
 // Do not edit below this line
@@ -11,3 +11,11 @@ module.exports = {
   ftoc,
   ctof
 };
+
+
+ftoc(32);
+ftoc(100);
+ftoc(-100);
+ctof(0);
+ctof(73.2);
+ctof(-10);
