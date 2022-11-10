b3bc5d93763d0e63fc9d57599b849ea6eec52a49
simca09 <delacruz.klydean@yahoo.com>
2022-11-09 19:17:22 -0500

    Easy peasy, jk it was pretty tough

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+const array = args[0];
+
+return array.filter(val => !args.includes(val))
+
+/* another way
+const newArray = [];
+array.forEach((item) => {
+    if (!args.includes(item)) {
+        newArray.push(item);
+    }
+});
+return newArray; */
+
 
 };
+
+removeFromArray([1,2,3,4], 3);
+removeFromArray([1,2,3,4], 3, 2);
+removeFromArray([1,2,3,4], "tacos");
+removeFromArray([1,2,3,4], 7, 2);
+removeFromArray([1,2,3,4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1,2,3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
