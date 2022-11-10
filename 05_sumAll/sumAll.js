c0073aa4cbd42e4d649a1c63441992cfcddeecc1
simca09 <delacruz.klydean@yahoo.com>
2022-11-10 09:44:10 -0500

    Changed to for loop instead of Gauss equation

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum = 0;
+    let max = 0;
+    let min = 0;
+    if (typeof(a) !== "number" || typeof(b) !== "number") {
+        return "ERROR";
+    } else if (a < 0 || b < 0 ) {
+        return "ERROR";
+    }
+    else if (a < b) {
+        min = a;
+        max = b;
+    } else if (b < a) {
+        min = b;
+        max = a;
+    } 
+for (i = min; i <= max; i++){
+    sum += i;
+}
+return sum;
+    }
 
-};
+sumAll(1,4);
+sumAll(1,4000);
+sumAll(123, 1);
+sumAll(-10, 4);
+sumAll(10, "90");
+sumAll(10, [90,1]);
 
 // Do not edit below this line
 module.exports = sumAll;
