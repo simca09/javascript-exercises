5bf5b741492216cf2e6e96de3952d56705f25c93
simca09 <delacruz.klydean@yahoo.com>
2022-11-10 10:35:26 -0500

    Used % instead of /

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+if (year % 400 == 0 && year % 4 == 0) {
+    return true;
+} else if (year % 100 == 0) {
+    return false;
+} else if (year % 4 == 0) {
+    return true;
+} else {
+    return false;
+}
+}
 
-};
+leapYears(1996);
+leapYears(1997);
+leapYears(34992);
+leapYears(1900);
+leapYears(1600);
+leapYears(700);
 
 // Do not edit below this line
 module.exports = leapYears;
