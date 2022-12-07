9f70374b6da15a83bf789498aeb7dfb8ba7a17b9
simca09 <delacruz.klydean@yahoo.com>
2022-12-07 11:47:09 -0500

    new Date() function was needed if oldest is still living

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+return array.reduce((oldAf, newAf) => {
+const oldAge =  getAge(oldAf.yearOfDeath, oldAf.yearOfBirth);
+const newAge = getAge(newAf.yearOfDeath, newAf.yearOfBirth);
 
+return oldAge > newAge ? oldAf : newAf || oldAge < newAge ? newAf : oldAf;
+})
 };
+
+const getAge = function(death, birth) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
